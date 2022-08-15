cdafe7d0121d1a4c6f2af9c3bd2d76e13b68631b
Praveen kumar <109021765+16pravikumar@users.noreply.github.com>
2022-08-15 19:56:38 +0530

    Create coll1.java

---
+++ coll1.java
@@ -0,0 +1,14 @@
+//Write a Java program to create a new array list, add some colors (string) and print out the collection. 
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    List<String> list=new ArrayList<String>();
+    list.add("Red");
+     list.add("White");
+    System.out.println(list);
+  }
+}
+
+/*OutPut
+  [Red, White]
+*/
